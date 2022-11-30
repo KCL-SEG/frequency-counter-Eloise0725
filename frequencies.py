ae1b29c3b88a880673748e98904bf5654baa7d7d
Ziwen Wang <eloisewong0725@gmail.com>
2022-11-30 03:08:34 +0000

    frequencies

--- frequencies.py
+++ frequencies.py
@@ -4,4 +4,10 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    for a in items:
+        a = str(a)
+        if a not in frequencies:
+            frequencies[a] = 1
+        else:
+            frequencies[a] += 1
     return frequencies
